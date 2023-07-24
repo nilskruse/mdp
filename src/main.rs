52878b06c9cca1162e00af1150dd3f80aee54164
Nils Kruse <nils.kruse@stud.uni-due.de>
2023-07-24 10:58:01 +0200

    feat: add multiagent files

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ use mdp::experiments;
 fn main() {
     // run_benchmarks();
     // experiments::cliff_walking::run_cliff_walking();
-    // experiments::cliff_walking::run_slippery_cliff_walking();
+    experiments::cliff_walking::run_slippery_cliff_walking();
     // experiments::cliff_walking::run_cliff_walking_episodic();
     // test();
     // visualisation::vis_test();
-    experiments::non_contractive::run_experiment();
+    // experiments::non_contractive::run_experiment();
     // experiments::q_learning_dynamic::run_experiment();
     // experiments::intersection::run_experiment();
     // experiments::q_learning_beta::run_q_beta_experiment();
